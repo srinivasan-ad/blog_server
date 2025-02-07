03ffc0140627b38051de54b1cfce1a1eefcd8008
srinivasan-ad <forprogramingpurpose@gmail.com>
2025-02-07 13:16:49 +0530

    blog

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,13 @@
-import express from 'express';
+import express, { NextFunction } from 'express';
 import cors from 'cors'
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
+import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv'; 
 dotenv.config();
 import {Pool} from 'pg'
 const app : express.Application = express();
+app.use(cookieParser())
 app.use(cors());
 app.use(express.json());
 const pool = new Pool({connectionString : process.env.DATABASE_URL})
@@ -15,6 +17,28 @@ const pool = new Pool({connectionString : process.env.DATABASE_URL})
       client.release();
   })
   .catch(err => console.error("Database connection failed", err));
+
+async function CheckTokenValidity(req : any ,res : any ,next : any) : Promise<any>
+{
+const token =   req.cookies?.authToken;
+try{
+    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY as string);
+    const selectQuery = 'SELECT * FROM Users WHERE username = $1;'
+    const result = await pool.query(selectQuery,[decoded]);
+    if(result.rows.length === 0)
+    {
+        return res.status(404).json({isValid : false , mssg : "token has expired !"})
+    }
+    next();
+    return res.status(202).json({isValid : true , mssg : "token is valid !"})
+
+}
+catch(e)
+{
+    console.error(e);
+    return res.status(500).json({mssg : "Internal Server Error"})
+}
+}
 
 app.get('/hello'  , (req,res) => {
     try{
@@ -29,6 +53,7 @@ app.get('/hello'  , (req,res) => {
 app.post('/user/signup', async (req , res ) : Promise<any> => {
 
 const {name,username,password} = req.body
+
     try {
         const selectQuery = `Select * FROM Users WHERE username = $1 ;`;
         const selectResult = await pool.query(selectQuery,[username]);
@@ -44,9 +69,9 @@ const {name,username,password} = req.body
             if(insertQueryResult.rowCount === 0)
             {
                 console.log('Query insertion failed !');
-              return res.status(400).json({success : false , exist : false , mssg  : 'Insertion failed' } )
+              return res.status(401).json({success : false , exist : false , mssg  : 'Insertion failed' } )
             }
-            let token = jwt.sign({ username: username }, process.env.JWT_SECRET_Key as string);
+            let token = jwt.sign({ username: username }, process.env.JWT_SECRET_KEY as string);
         return res.status(200).json({success : true , exist : false , mssg : 'Info inserted successfully'}).cookie("authToken", token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production', 
@@ -60,9 +85,21 @@ const {name,username,password} = req.body
         return res.status(500).json({ success: false, error: "Internal Server Error" });
     }
 });
-app.post('/user/signin', async(req,res) : Promise<any>  =>  {
+app.post('/user/signin',async(req,res) : Promise<any>  =>  {
     const{username,password} = req.body 
+    const token =   req.cookies?.authToken;
     try{
+        if(token)
+            {
+                const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY as string) as jwt.JwtPayload & { username: string };;
+                const tokenSelectQuery = 'SELECT * FROM Users WHERE username = $1;'
+                const result = await pool.query(tokenSelectQuery,[decoded.username]);
+                if(result.rows.length > 0)
+                {
+                    return res.status(210).json({isValid : true , mssg : "token is valid !"})
+                }
+            
+            }
         const selectQuery = 'SELECT * FROM Users WHERE username = $1 ;'
         const selectQueryResult = await pool.query(selectQuery , [username])
         if(selectQueryResult.rows.length === 0)
@@ -75,7 +112,12 @@ app.post('/user/signin', async(req,res) : Promise<any>  =>  {
         {
             return res.status(405).json({mssg : 'Password is incorrect'}) 
         }
-            return res.status(201).json({mssg : 'Login is successful'})  
+        let newToken = jwt.sign({ username: username }, process.env.JWT_SECRET_KEY as string);
+        return res.status(200).json({mssg : 'Logged in successfully'}).cookie("authToken", token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production', 
+            maxAge: 3 * 24 * 60 * 60 * 1000
+        });
     }
     catch(e)
     {
